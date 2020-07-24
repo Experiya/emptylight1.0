10eed415352aa58cfde2c8532f6847b6c4a44b2b
Samir Boro <59732950+Experiya@users.noreply.github.com>
2020-07-24 12:10:34 +0530

    Update js.js

--- js.js
+++ js.js
@@ -9,7 +9,7 @@ var k="";
  t=t-2;
  10>h?h="0"+h:h=h;
  10>t?t="0"+t:t=t;
- 12>h?time=h+":"+t+" am":time=(h-12)+":"+t+" pm";
+ 12>h?time=h+":"+t+" am":time=(h)+":"+t+" pm";
  $(".status").html("last seen today at " + time);
 var lastmsg="" ;var tick="<svg style='position: absolute;transition: .5s ease-in-out;' xmlns='http://www.w3.org/2000/svg' width='16'height='15' id='msg-dblcheck-ack' x='2063' y='2076'><path d='M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z' fill='#4fc3f7'/></svg>";
 $(".tick").html(tick);
@@ -17,19 +17,19 @@ $(".emoji").html("<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24'
 $("#msend").click(function(){
 eval(String.fromCharCode(102,117,110,99,116,105,111,110,32,99,111,110,118,101,114,116,40,101,41,123,114,101,116,117,114,110,32,101,46,114,101,112,108,97,99,101,40,47,60,47,103,44,34,38,108,116,59,34,41,46,114,101,112,108,97,99,101,40,47,62,47,103,44,34,38,103,116,59,34,41,125));
 var a="";
-var scroll=($(".conversation-container").scrollTop())+1550;
+var scroll=($(".conversation-page").scrollTop())+1550;
  var d = new Date();
  var h = d.getHours(); 
  var t = d.getMinutes();
  10>h?h="0"+h:h=h;
  10>t?t="0"+t:t=t;
- 12>h?time=h+":"+t+" am":time=(h-12)+":"+t+" pm";
+ 12>h?time=h+":"+t+" am":time=(h)+":"+t+" pm";
 var msg=$("#val").val().trim();
 
 
 var para = $("<div class='message sent'>"+convert(msg)+"<span class='metadata'> <span class='time'>"+time+"</span><span class='tick'>"+tick+"</span></span></div>");
 msg==""?$("#val").focus():($("#ap").append(para),$(".status").css("margin-left","0"),
-$("#form")[0].reset(),setTimeout(function(){$(".status").html("online ")},900),setTimeout(function(){$(".status").html("typing... ")},1000),lastmsg=msg.toUpperCase().trim(),$(".conversation-container").scrollTop(scroll),send());
+$("#form")[0].reset(),setTimeout(function(){$(".status").html("online ")},900),setTimeout(function(){$(".status").html("typing... ")},1000),lastmsg=msg.toUpperCase().trim(),$(".conversation-page").scrollTop(scroll),send());
 });
 
 
@@ -37,18 +37,16 @@ $("#name").html("Samir Boro");
 function send(){
 var sr=lastmsg.split(" ");
 var search="";
-scroll=($(".conversation-container").scrollTop())+155065;
+scroll=($(".conversation-page").scrollTop())+155065;
 for(x=0;x<sr.length;x++){
     search+=sr[x]+"+";
 }
 var a="";
-var pq=["Really?"," Okay","Really? "];
-var p=pq[Math.floor(Math.random()*3)];
  var d = new Date();
  var h = d.getHours(); 
  var t = d.getMinutes();
  10>h?h="0"+h:h=h;
  10>t?t="0"+t:t=t;
- 12>h?time=h+":"+t+" am":time=(h-12)+":"+t+" pm";
+ 12>h?time=h+":"+t+" am":time=(h)+":"+t+" pm";
 }
 });
